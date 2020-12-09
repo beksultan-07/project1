ae951ae2728332d438ce5c4f475ad1d6d3c50177
Bekas <bakytuulubeksultan@gmal.com>
2020-12-09 01:25:52 +0100

    signIn1

--- scritpt.js
+++ scritpt.js
@@ -14,12 +14,6 @@ new Swiper('.image-slider', {
     autoHeight: true,
   });
 
-// register siugn-in
-var signInCont = document.querySelector(".signInCont");
-  signInCont.addEventListener("click", function() {
-    signInCont.classList.toggle("active");
-});
-
 // hamburger
 var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
@@ -30,11 +24,41 @@ hamburger.addEventListener("click", function() {
 let popup = document.querySelector('#popap');
 var popap__close = document.querySelector(".popap__close");
 popap__close.addEventListener("click", function() {
-  
   popup.style.opacity = 0;
   setTimeout(() => {
     popup.remove();
   }, 500)
 });
 
+
+// register siugn-in
+function closeP(op, vis, bt){
+  popup2.style.opacity = op;
+  popup2.style.visibility = vis;
+  popup2.style.bottom = bt;
+}
+
+var username = document.querySelector("#username");
+var buttonText = document.querySelector(".sign-button");
+var signClose = document.querySelector(".popup2__area");
+var popup2 = document.getElementById('popup2')
+
+var loginEmail = 'login@email.com';
+
+var signInCont = document.querySelector(".signInCont");
+
+signInCont.addEventListener("click", function() {
+  if(username.value === '' && username.value !== loginEmail && !signInCont.classList.contains('active')){
+    buttonText.textContent = "Register"   
+    closeP(1, 'visible', 0)
+
+  } else {
+    buttonText.textContent = "Login"
+  }
+
+  if(username.value === loginEmail) {
+    buttonText.textContent = "Password"
+  }
+  signInCont.classList.toggle("active");
+});
 
